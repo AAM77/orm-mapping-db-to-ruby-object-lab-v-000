ec8e9cada62097b83a490b11e797df713256eaaf
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-17 21:55:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,9 +27,7 @@ class Student
       SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first #map |row|
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
   end #find_by_name
   
   
